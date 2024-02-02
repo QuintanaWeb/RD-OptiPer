b666906dcabaf50b70c9deebfaa7db5171c0c5f6
QuintanaWeb <114363080+QuintanaWeb@users.noreply.github.com>
2024-02-03 00:37:18 +0100

    Update funtions.php

--- funtions.php
+++ funtions.php
@@ -32,3 +32,9 @@ function wptutsplus_admin_styles() {
     wp_enqueue_style( 'wptuts_admin_stylesheet' );
 }
 add_action( 'admin_enqueue_scripts', 'wptutsplus_admin_styles' );
+
+// CAMBIAR PIE ADMINISTRACIÓN
+function footer_admin_wp() {
+    return '<b>Somos <a target="_blank" href="https://www.reseteodigital.es">ReseteoDigital.es</a> | Oficina: <a target="_blank" href="tel:+34670907657">670907657</a> | Soporte por WhatsApp: <a target="_blank" href="https://api.whatsapp.com/send?phone=34644062375&text=%C2%A1Necesito%20ayuda%20con%20mi%20web!">644062375</a> | Email: <a target="_blank" href="mailto:soporte@reseteodigital.es">soporte@reseteodigital.es</a></b>';
+    }
+    add_action( 'admin_footer_text', 'footer_admin_wp' );
